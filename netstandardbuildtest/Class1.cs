3b8d4cf4db0d33b7bbb74f21499177dc4d212cee
Toni Petrina <tpetrina@tpetrina.com>
2017-07-11 19:50:48 +0200

    Playing with platform specific code

--- netstandardbuildtest/Class1.cs
+++ netstandardbuildtest/Class1.cs
@@ -4,5 +4,13 @@ namespace netstandardbuildtest
 {
     public class Class1
     {
+        public string Say()
+        {
+#if NETSTANDARD1_0
+            throw new NotSupportedException("This PCL assembly must not be used at runtime..");
+#else
+            return Message.Greeting;
+#endif
+        }
     }
 }
---
+++ netstandardbuildtest/Platforms/Other/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace netstandardbuildtest
+{
+    class Message
+    {
+        public static string Greeting = "Other";
+    }
+}
